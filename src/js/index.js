638a44882f3c545be3c6cd4394bb3d4b618b28ee
RABBI ISLAM <ronya4927@gmail.com>
2023-12-02 18:53:12 +0600

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+import "../css/style.css";
+
+function content_ready_scripts() {
+
+}
+
+window.addEventListener('scroll', function(e) {
+ 
+ });
+
+
+
+// Document Loaded
+document.addEventListener("DOMContentLoaded", () => {
+  content_ready_scripts();
+});
